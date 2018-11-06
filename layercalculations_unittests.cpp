d1b0ede6b724f303180751b303562228f5757658
Taylor Greenwood <taylorgwood@gmail.com>
2018-11-06 08:29:04 -0700

    Added unit tests

---
+++ layercalculations_unittests.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+
+//void EXPECT_EQ_COMPLEX(const Complex& firstObject, const Complex& secondObject)
+//{
+//    EXPECT_DOUBLE_EQ(firstObject.get_real(), secondObject.get_real());
+//    EXPECT_DOUBLE_EQ(firstObject.get_imaginary(), secondObject.get_imaginary());
+//}
+
+//TEST(ComplexConstructor, whenGivenTwoNumbers_getComplexNumber)
+//{
+//    double expectedReal{1};
+//    double expectedImaginary{2};
+//    Complex complexNumber{expectedReal,expectedImaginary};
+
+//    EXPECT_DOUBLE_EQ(expectedReal, complexNumber.get_real());
+//    EXPECT_DOUBLE_EQ(expectedImaginary, complexNumber.get_imaginary());
+//}
